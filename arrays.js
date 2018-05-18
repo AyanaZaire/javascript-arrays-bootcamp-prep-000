6931821c62f7a473c2506e2f2a682bab2e650251
AyanaZaire <ayanazaire@gmail.com>
2018-05-18 21:38:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,13 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(){
+  var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
   
+  chocolateBars.unshift("reeses")
+  
+  return chocolateBars
 }
 
-function 
+function destructivelyAddElementToBeginningOfArray (){
+  
+}
