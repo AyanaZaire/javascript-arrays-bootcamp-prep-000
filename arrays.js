8f343219aec7f9fb9d2f6a66b8b569fd7928b5c2
AyanaZaire <ayanazaire@gmail.com>
2018-05-18 22:20:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElemenToEndOfArray(array, element){
- return [array, ...element]
+ return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
